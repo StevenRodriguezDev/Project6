a2c04b73c5c0c01f9c45b22fe2e31d308d87d598
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2022-01-13 16:40:48 -0700

    added needed variables

---
+++ game_show_app_v1.2/app.js
@@ -0,0 +1,7 @@
+// add needed variables
+const qwerty = document.getElementById('qwerty');
+const phase = document.getElementById('phrase');
+const StartGame = document.getElementsByClassName('btn__reset');
+
+let missed = 0;
+
