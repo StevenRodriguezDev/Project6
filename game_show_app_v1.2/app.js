db256ba71474b00e86a863911a53697b29c09046
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2022-01-17 17:58:54 -0700

    updated js

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -5,3 +5,43 @@ const StartGame = document.getElementsByClassName('btn__reset');
 
 let missed = 0;
 
+//Array named Phrases
+const phrases =  [
+    'Remember why you started',
+    'Do what you love',
+    'Get better every single day',
+    'It is the will not the skill',
+    'Trust the process'
+];
+
+//return a random phrase from an array 
+const getRandomPhraseAsArray = arr => {
+
+
+
+}
+
+// add letters of a string to the display 
+const addPhraseToDisplay = arr => {
+
+}
+
+//checks if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+//checks if the game has been won or lost
+const checkWin = () => {
+
+}
+
+//listen for the start of the game button to be pressed
+startButton.addEventListener('click',() => {
+
+});
+
+//listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+});
